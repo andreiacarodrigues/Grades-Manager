64d457cefb2fb3da010f93269aa72ef9c8b206b6
xCutePsycho <andreiacarodrigues@gmail.com>
2017-02-06 19:30:22 +0000

    wow tem titulo igual ao outro

--- app/src/main/java/com/acr/gm/gradesmanager/DisciplineActivity.java
+++ app/src/main/java/com/acr/gm/gradesmanager/DisciplineActivity.java
@@ -1,9 +1,15 @@
 package com.acr.gm.gradesmanager;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class DisciplineActivity extends AppCompatActivity {
 
@@ -12,9 +18,31 @@ public class DisciplineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discipline);
 
-       Intent intent = getIntent();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
         String message = intent.getStringExtra("discipline");
 
         Log.i("IMPORTANT", message);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.plus2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Retira o titulo por default
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        // Coloca nova font no titulo
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/f&c.ttf");
+        TextView tv = (TextView) findViewById(R.id.toolbar_title2);
+        tv.setTypeface(tf);
     }
 }
